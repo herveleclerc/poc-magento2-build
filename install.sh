51334159737cba5c700773d26023985c485561fd
herveleclerc <herve.leclerc@gmail.com>
2022-11-15 16:38:14 +0100

    test downgrade

--- install.sh
+++ install.sh
@@ -6,9 +6,17 @@ COMPOSER_MEMORY_LIMIT=-1 composer selfupdate --1
 
 COMPOSER_MEMORY_LIMIT=-1 composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=${MAGENTO_VERSION} src
 
+
+
 echo "Installing CI/CD packages..."
 cd src
-#composer require phoenix-media/magento2-cloud-build --no-update
-#composer update
-#vendor/bin/px-cloud-build-install
+
+COMPOSER_MEMORY_LIMIT=-1 composer require --no-update --dev magento/ece-tools magento/magento-cloud-docker
+
+composer require herleclerc/magento2-cloud-build --no-update
+
+composer update
+
+vendor/bin/px-cloud-build-install
+
 cd ..
